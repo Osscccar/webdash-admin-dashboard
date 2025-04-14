4d24a57ffe76766dc472d804dc068b3af46c4f82
Osscccar <Oscarvonbergen@gmail.com>
2025-04-14 14:18:01 +1000

    Fixed client detail import

--- src/app/dashboard/client/[id]/page.tsx
+++ src/app/dashboard/client/[id]/page.tsx
@@ -1,5 +1,5 @@
 // src/app/dashboard/client/[id]/page.tsx
-import ClientDetail from './client-detail';
+import ClientDetail from '@/components/client-detail';
 
 // Correctly type the params object according to Next.js expectations
 export default function ClientPage({ params }: { params: { id: string } }) {
